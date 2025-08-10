5df343ac4cf58d011ab308e5c674b66460dba914
Gabrielbrta <vdvsa159946@gmail.com>
2025-08-10 17:09:41 -0300

    backend

--- js/app.js
+++ js/app.js
@@ -1,119 +1,130 @@
-import { setFirtsFiveItems } from "./filmesIniciais.js";
-import { saveFilmes, getFilmes } from "./form.js";
 
 const cardContainer = document.querySelector('.cards');
+const API_URL = "http://localhost:3000";
 
+// Busca todos os filmes do servidos
+async function getServerFilmes() {
+    try {
+        const data = await fetch(`${API_URL}/filmes`)
+        const json = await data.json();
+        return json;
 
-// Cria o objeto dos itens
-function createFilme() {
-    const filmes = getFilmes();
-
-// após salvar ele cria o card do filme
-    filmes.forEach((filme) => {
-        createCard(filme.id,
-            filme.titulo,
-            filme.genero,
-            filme.descricao,
-            filme.imagem,
-            filme.gostei,
-            filme.naoGostei,
-    
-        );
-    })
+    } catch (error) {
+        console.log("Houve um erro na requisição");
+    }
 }
 
 // Função que cria os cards dos filmes/series
-function createCard(id, titulo, genero, descricao, imagem, gostei, naoGostei) {
-    let htmlCard =`
-        <div class="card" id="id${id}">
-            <div class="title">
-            <h2>${titulo}</h2>
+async function createCard() {
+    const dataCard = await getServerFilmes();
+
+    dataCard.forEach((filme) => {
+        let htmlCard =`
+            <div class="card" id="id${filme.id}">
+                <div class="title">
+                <h2>${filme.titulo}</h2>
+                </div>
+                <div class="gender">
+                    <p><strong>Gênero:</strong> ${filme.genero}</p>
+                </div>
+                <div class="img">
+                    <img src="${filme.imagem}" onerror="this.onerror=null; this.src='https://picsum.photos/800/600';" alt="${filme.titulo}">
+                </div>
+                <div class="descricao">
+                    <p><strong>Descrição:</strong></p>
+                    <p class="description"> ${filme.descricao != '' ? filme.descricao : 'Sem descrição'}</p>
+                </div>
+                <ul class="feedback-list">
+                    <li class="gostei${filme.id}"> <strong>Gostaram: </strong>${filme.gostei}</li>
+                    <li class="naoGostei${filme.id}"> <strong>Não gostaram: </strong>${filme.naoGostei}</li>
+                    <li class="total${filme.id}"> <strong>Total de votos: </strong>${filme.gostei + filme.naoGostei}</li>
+                </ul>
+                <div class="buttons">
+                    <button type="button" class="like" data-feedback='${filme.id}'>Gostei</button>
+                    <button type="button" class="dislike" data-feedback='${filme.id}'>Não Gostei</button>
+                </div>
             </div>
-            <div class="gender">
-                <p><strong>Gênero:</strong> ${genero}</p>
-            </div>
-            <div class="img">
-                <img src="${imagem}" onerror="this.onerror=null; this.src='https://picsum.photos/800/600';" alt="${titulo}">
-            </div>
-            <div class="descricao">
-                <p><strong>Descrição:</strong></p>
-                <p class="description"> ${descricao != '' ? descricao : 'Sem descrição'}</p>
-            </div>
-            <ul class="feedback-list">
-                <li class="gostei${id}"> <strong>Gostaram: </strong>${gostei}</li>
-                <li class="naoGostei${id}"> <strong>Não gostaram: </strong>${naoGostei}</li>
-                <li class="total${id}"> <strong>Total de votos: </strong>${gostei + naoGostei}</li>
-            </ul>
-            <div class="buttons">
-                <button class="like" data-feedback='${id}'>Gostei</button>
-                <button class="dislike" data-feedback='${id}'>Não Gostei</button>
-            </div>
-        </div>
-    `
-    if(cardContainer) {
-        cardContainer.insertAdjacentHTML('afterbegin',htmlCard); 
-        votar(document.querySelectorAll(`[data-feedback="${id}"]`))
-    } else {
-        console.log("Erro ao inserir os cards");
-    }
+        `
+
+        if(cardContainer) {
+            cardContainer.insertAdjacentHTML('afterbegin',htmlCard); 
+            votar(document.querySelectorAll(`[data-feedback="${filme.id}"]`))
+        } else {
+            console.log("Erro ao inserir os cards");
+        }
+    });
 }
 
 // adiciona o evento de clique no botão dos cards
 function votar(buttons) {
     buttons.forEach((btn) => {
-        btn.addEventListener('click', (e) => {
-            const filmes = getFilmes();
+        btn.addEventListener('click', async (e) => {
+            e.preventDefault();
+            const value = btn.classList.contains("like") ? "like" : "dislike";
+
             let btnId = btn.dataset.feedback;
+
+            // elementos do dom
             const totalItem = document.querySelector(`.total${btnId}`);
             const gosteiItem = document.querySelector(`.gostei${btnId}`);
             const naoGosteiItem = document.querySelector(`.naoGostei${btnId}`);
             
-            const filme = filmes.find(f => f.id == btnId);
-    
-        //incrementa os valores de gostei e não gostei
-            if(e.target.classList == "like") {
-                filme.gostei++
-                saveFilmes(filmes);
-                //atualiza o paragrafo gostei ao clicar no botão
-                gosteiItem.innerHTML= `<strong>Gostaram: </strong>${filme.gostei}`;
 
-                //atualiza o total de votos por item ao clicar
-                totalItem.innerHTML = `<strong>Total de votos: </strong>${filme.gostei + filme.naoGostei}`
+            //incrementa os valores de gostei e não gostei
+            if(e.target.classList == "like") {
+                //atualiza o paragrafo gostei ao clicar no botão
+                const atual = parseInt(gosteiItem.innerHTML.replace(/\D/g, "")) + 1;
+                gosteiItem.innerHTML = `<strong>Gostaram: </strong>${atual}`;
+                
             };
             if(e.target.classList == "dislike") {
-                filme.naoGostei++
-                saveFilmes(filmes);
-                //atualiza o paragrafo gostei ao clicar no botão
-                naoGosteiItem.innerHTML= `<strong>Não gostaram: </strong>${filme.naoGostei}`;
-                
-                //atualiza o total de votos por item ao clicar
-                totalItem.innerHTML = `<strong>Total de votos: </strong>${filme.gostei + filme.naoGostei}`
+                //atualiza o paragrafo não gostei ao clicar no botão
+                const atual = parseInt(naoGosteiItem.innerHTML.replace(/\D/g, "")) + 1;
+                naoGosteiItem.innerHTML = `<strong>Não gostaram: </strong>${atual}`;
             };
             
+            const atualTotal = parseInt(totalItem.innerHTML.replace(/\D/g, "")) + 1;
+            totalItem.innerHTML = `<strong>Total de votos: </strong>${atualTotal}`
+
+            // registra os votos
+            try {
+                fetch(`${API_URL}/voto/${btnId}`,{
+                    method: "POST",
+                    headers: {"Content-type": "application/json"},
+                    body: JSON.stringify({value}),
+                });
+            } catch(error) {
+                console.log("erro ao registrar voto: ", error);
+            }
+            
+            // registra os votos totais
             setTotalFeedbacks();
         })
     })
     
 }
+
 // pega os valores totais de likes;
-function setTotalFeedbacks() {
-    const filmes = getFilmes();
-    const totalGostei = filmes.reduce((acc, filme) => {  return acc + filme.gostei;}, 0);
-    const totalNaoGostei = filmes.reduce((acc, filme) => {  return acc + filme.naoGostei;}, 0);
-    const totalVotos = totalGostei + totalNaoGostei;
-    
-    document.querySelector('.totalFeedback').innerHTML = `
-    <li>Total de votos em gostei: <span class='stats'>${totalGostei}</span></li>
-    <li>Total de votos em não gostei: <span class='stats'>${totalNaoGostei}</span></li>
-    <li>Total de votos: <span class='stats'>${totalVotos}</span></li>
-`
+async function setTotalFeedbacks() {
+    //busca os votos totais pela api
+    try{
+        let positivos = await fetch(`${API_URL}/contador/positivos`).then((total) => total.json());
+        let negativos = await fetch(`${API_URL}/contador/negativos`).then((total) => total.json());
+        let totalVotos = positivos.totalPositivos + negativos.totalNegativos;
+
+        document.querySelector('.totalFeedback').innerHTML = `
+        <li>Total de votos em gostei: <span class='stats'>${positivos.totalPositivos}</span></li>
+        <li>Total de votos em não gostei: <span class='stats'>${negativos.totalNegativos}</span></li>
+        <li>Total de votos: <span class='stats'>${totalVotos}</span></li>
+        `
+    }catch(error) {
+        console.log("Erro na requisição dos totais: " + error);
+    }
     
 }
-// adiciona os 5 primeiros itens
 
 // Cria os cards e insere no html assim que ele é carregado
 document.addEventListener('DOMContentLoaded', async () => {
-    await setFirtsFiveItems();
-    createFilme();
-    setTotalFeedbacks();
+    await createCard();
+    await setTotalFeedbacks();
 })
--- js/form.js
+++ js/form.js
@@ -1,65 +1,35 @@
 const craeteBtn = document.querySelector('#create');
-const logoBtn = document.querySelector('.logo a');
+const logoBtn = document.querySelector('a');
+const API_URL = "http://localhost:3000";
 
 // se existir o botão ele adiciona um evento de click
 if(craeteBtn) {
-    craeteBtn.addEventListener('click', (e) => handleClick(e));
+    craeteBtn.addEventListener('click', async (e) => handleClick(e));
 }
 
 // verifica os dados do formulario e adiciona o filme no backend(localstorage)
-function handleClick(e) {
+async function handleClick(e) {
     e.preventDefault();
     const form = document.querySelector('form');
     let error = document.querySelector('.error');
-    let title = form.title.value;
-    let gender = form.gender.value;
-    let image = form.image.value;
-    let description = form.description.value;
+    let title = form.titulo.value;
+    let gender = form.genero.value;
+    let image = form.imagem.value;
+
     
     // verifica se o usuário inseriu todos os campos obrigatórios
     if(title && gender && image) {
-        error.innerText = "";
-        createFilme('',title, gender, description, image);
-        feedback('criado');
-        if(logoBtn) logoBtn.click();
+        const formData = new FormData(form);
+        const data = Object.fromEntries(formData.entries())
+        await fetch(`${API_URL}/filmes`, {
+            method: "POST",
+            headers: {"Content-Type": "application/json"},
+            body: JSON.stringify(data),
+        });
+        logoBtn.click();
     } else {
-        feedback(false);
+        alert("Ocorreu um erro")
         error.innerText = "Preencha todos os campos!";
     }
     
 }
-
-function feedback(state) {
-    state == 'criado' ? alert('filme adicionado com sucesso!') : alert("Ocorreu um erro");
-}
-
-// Funções do localstorage ---------------------------
-
-// seleciona todos os filmes que existirem no backend, caso não tenha ele retorna um array vazio
-export function getFilmes() {
-    return JSON.parse(localStorage.getItem('filmes')) || [];
-}
-
-// salva os filmes no backend como um dado json
-export function saveFilmes(filmes) {
-    localStorage.setItem('filmes', JSON.stringify(filmes));
-}
-
-// adiciona os filmes no localstorage
-export function createFilme(id, titulo, genero, descricao = '', imagem,) {
-    const filmes = getFilmes();
-    filmes.push({
-        id: id == '' ? Date.now(): id,
-        titulo,
-        genero,
-        descricao,
-        imagem,
-        gostei: 0,
-        naoGostei: 0,
-    }); 
-
-// salva os filmes no localStorage("backend")
-    saveFilmes(filmes);
-}
-
-
